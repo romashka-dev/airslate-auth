e250e239defca6ee63419633e29c11b3326652a9
Roman Holovizin <romanholovizin@Romans-MacBook-Pro.local>
2024-11-18 22:53:20 +0200

    [ui]: add styles for login form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -42,68 +42,72 @@ function App() {
     <>
       <div className="layout">
         <div className="layout__header">
-          <img src="./images/svg/airslate-mono.svg" width={150} height={48} alt="airSlate logo" />
+          <div className="main-logo">
+            <img src="./images/svg/airslate-mono.svg" width={150} height={48} alt="airSlate logo" />
+          </div>
         </div>
         <main className="layout__body">
           <form action="/" className="login-form" method="post" name="form" autoComplete="off">
-            <h1 className="login-form__title">Log in to airSlate</h1>
-            <div className="login-form__controls">
-              <div className="form-group">
-                <div className="form-group__row">
-                  <div className="form-group__items">
-                    <InputGroup
-                      labelProps={{
-                        selector: "form-group__label",
-                        text: "Enter an email",
-                        htmlFor: "Email"
-                      }}
-                      inputProps={{
-                        id: "Email",
-                        className: "form-group__input",
-                        type: "email",
-                        name: "Email address",
-                        placeholder: "Enter an email",
-                        autoComplete: "email"
-                      }}
-                    />
+            <div className="login-form__inner">
+              <h1 className="login-form__title">Log in to airSlate</h1>
+              <div className="login-form__controls">
+                <div className="form-group">
+                  <div className="form-group__row">
+                    <div className="form-group__items">
+                      <InputGroup
+                        labelProps={{
+                          selector: "form-group__label",
+                          text: "Enter an email",
+                          htmlFor: "Email"
+                        }}
+                        inputProps={{
+                          id: "Email",
+                          className: "form-group__input",
+                          type: "email",
+                          name: "Email address",
+                          placeholder: "Enter an email",
+                          autoComplete: "email"
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="form-group__row">
-                  <div className="form-group__items">
-                    <InputGroup
-                      labelProps={{
-                        selector: "form-group__label",
-                        text: "Password",
-                        htmlFor: "Password"
-                      }}
-                      inputProps={{
-                        id: "Password",
-                        className: "form-group__input",
-                        type: "password",
-                        name: "Password",
-                        placeholder: "Enter password",
-                        autoComplete: "password"
-                      }}
-                    />
+                  <div className="form-group__row">
+                    <div className="form-group__items">
+                      <InputGroup
+                        labelProps={{
+                          selector: "form-group__label",
+                          text: "Password",
+                          htmlFor: "Password"
+                        }}
+                        inputProps={{
+                          id: "Password",
+                          className: "form-group__input",
+                          type: "password",
+                          name: "Password",
+                          placeholder: "Enter password",
+                          autoComplete: "password"
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="form-group__row">
-                  <p className="policy">
-                    By clicking Log in, you agree to the
-                    <a className="policy__Link" href="https://www.airslate.com/terms-of-service" target="_blank">Terms of Services</a>
-                    <span className="policy__text">and</span>
-                    <a className="policy__Link" href="https://www.airslate.com/privacy-notice" target="_blank">Privacy Policy,</a>
-                    <span className="policy__text">including receipt of emails from of us about our service.</span>
-                  </p>
-                </div>
-                <div className="form-group__row">
-                  <div className="form-group__action">
-                    <Button
-                      title="Log in"
-                      selector="form-group__button"
-                      type="submit"
-                      ariaLabel="Log in"
-                    />
+                  <div className="form-group__row">
+                    <p className="policy">
+                      By clicking Log in, you agree to the&ensp;
+                      <a className="policy__Link" href="https://www.airslate.com/terms-of-service" target="_blank">Terms of Services</a>
+                      <span className="policy__text">&ensp;and&ensp;</span>
+                      <a className="policy__Link" href="https://www.airslate.com/privacy-notice" target="_blank">Privacy Policy,</a>
+                      <span className="policy__text">&ensp;including receipt of emails from of us about our service.</span>
+                    </p>
+                  </div>
+                  <div className="form-group__row">
+                    <div className="form-group__action">
+                      <Button
+                        title="Log in"
+                        selector="form-group__button"
+                        type="submit"
+                        ariaLabel="Log in"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -112,7 +116,7 @@ function App() {
         </main>
         <div className="layout__footer">
           <AuthorInfo
-            projectDescription="Project: Counter on ReactJS"
+            projectDescription="Project: airSlate Auth"
             authorName="Author: Holovizin Roman"
           />
           <LogoList logos={logos} />
